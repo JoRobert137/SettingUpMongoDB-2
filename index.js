9d8e89d5bced0abc1ed55164deae7a840a4b0f88
R John Robert <rjohn.robert@kalvium.community>
2025-01-08 12:56:45 +0530

    Assignment Completed

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose');
+const user = require('./schema.js')
+
+if (process.env.NODE_ENV !== 'PRODUCTION') {
+  require('dotenv').config({
+    path: './.env',
+  });
+}
 
 const app = express();
-const port = 3010;
+const port = process.env.PORT;
 
-app.use(express.static('static'));
+app.use(express.json());
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log('Connected to database'))
+  .catch((err) => console.error('Error connecting to database:', err));
+
+app.post('/api/users', async (req, res) => {
+  try {
+    const userData = req.body;
+
+    const newUser = new User(userData);
+    await newUser.save();
+
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      res
+        .status(400)
+        .json({ message: 'Validation error', details: error.message });
+    } else {
+      res.status(500).json({ message: 'Server error', details: error.message });
+    }
+  }
+});
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
